3dcc1426169ba717bbadd4701232392e86832344
Ravi <rjgadag@gmail.com>
2019-10-06 20:15:39 +0530

    Add project files.

---
+++ NetCoregRPC.Client.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using static NetCoregRPC.Server.Users;
+
+namespace NetCoregRPC.Client.ConsoleApp
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new UsersClient(channel);
+
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+            Console.WriteLine($"Sending User Request via gRPC");
+
+            using var streamingCall = client.GetAllUser(new Server.SearchRequest() { Name="R"}, cancellationToken: cts.Token);
+
+            try
+            {
+                Console.WriteLine("Response Recived from gRPC \n");
+                await foreach (var userData in streamingCall.ResponseStream.ReadAllAsync(cancellationToken: cts.Token))
+                {
+                    Console.WriteLine($" {userData.Id} | {userData.Name}");
+
+                }
+
+                Console.ReadLine();
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+            {
+                Console.WriteLine("Stream cancelled.");
+            }
+        }
+    }
+}
---
+++ NetCoregRPC.Server/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoregRPC.Server
+{
+    public class UserService:Users.UsersBase
+    {
+        private readonly ILogger<UserService> _logger;
+        public UserService(ILogger<UserService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task GetAllUser(SearchRequest request, IServerStreamWriter<Response> responseStream, ServerCallContext context)
+        {
+            var rng = new Random();
+            var now = DateTime.UtcNow;
+
+            var i = 0;
+            while (!context.CancellationToken.IsCancellationRequested && i < 2)
+            {
+                await Task.Delay(200); 
+
+                var forecast = new Response
+                {
+                    Id=1,
+                    Name="User 1 From gRPC Service"
+                };
+                _logger.LogInformation("Sending User response");
+                await responseStream.WriteAsync(forecast);
+            }
+        }
+    }
+}
